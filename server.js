10d69c99e4c7b63d86ac5ffddfac45a9fb44113b
Luke Southam <luke@devthe,com>
2016-12-29 00:36:11 +0000

    Removed double '/' from ssl redirect.

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ function App() {
 
     if(production) {
         app.get('*',function(req,res,next){
-          if(req.headers['x-forwarded-proto']!='https')
-            res.redirect('https://'+ req.headers.host + '/' + req.url);
+          if(req.headers['x-forwarded-proto'] != 'https')
+            res.redirect('https://'+ req.headers.host + req.url);
           else
             next();
         });
