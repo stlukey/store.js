815c40fbc6e16af1b96d32cb128b4bb9f06f26b1
Luke Southam <luke@devthe.com>
2017-06-24 00:13:21 +0100

    Added Card UI.

--- src/cart/cart.jsx
+++ src/cart/cart.jsx
@@ -12,11 +12,158 @@ import {fetchAll} from '../products/actions';
 import newMessage from '../messages/actions';
 import {Section, Title} from '../app/bulma';
 
+import Cards from 'react-credit-cards';
+import Dialog from 'material-ui/Dialog';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import 'react-credit-cards/lib/styles.scss';
+
+let loadedStripe = true;
+
+class Card  extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: '',
+      name: '',
+      exp: '',
+      cvc: '',
+      focused: '',
+    };
+  }
+
+  componentDidMount() {
+    Payment.formatCardNumber(document.querySelector('[name="number"]'));
+    Payment.formatCardExpiry(document.querySelector('[name="expiry"]'));
+    Payment.formatCardCVC(document.querySelector('[name="cvc"]'));
+  }
+
+  handleInputFocus = (e) => {
+    const target = e.target;
+
+    this.setState({
+      focused: target.name,
+    });
+  };
+
+  handleInputChange = (e) => {
+    const target = e.target;
+
+    if (target.name === 'number') {
+      this.setState({
+        [target.name]: target.value.replace(/ /g, ''),
+      });
+    }
+    else if (target.name === 'expiry') {
+      this.setState({
+        [target.name]: target.value.replace(/ |\//g, ''),
+      });
+    }
+    else {
+      this.setState({
+        [target.name]: target.value,
+      });
+    }
+  };
+
+  handleCallback(type, isValid) {
+    console.log(type, isValid); //eslint-disable-line no-console
+  }
+
+  render() {
+    const { name, number, expiry, cvc, focused } = this.state;
+    return (
+      <div className="rccs__demo">
+        <div className="rccs__demo__content">
+          <Cards
+            number={number}
+            name={name}
+            expiry={expiry}
+            cvc={cvc}
+            focused={focused}
+            callback={this.handleCallback}
+          />
+          <form>
+          <center>
+            <div>
+              <input
+                type="tel"
+                name="number"
+                placeholder="Card Number"
+                onKeyUp={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                onKeyUp={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />
+            </div>
+            <div>
+              <input
+                type="tel"
+                name="expiry"
+                placeholder="Valid Thru"
+                onKeyUp={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />
+              <input
+                type="tel"
+                name="cvc"
+                placeholder="CVC"
+                onKeyUp={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />
+            </div>
+            </center>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Payment extends Component {
+  state = {
+    dialogOpen: false,
+  };
+
+
+  openDialog = (type) => {
+    this.setState({
+      dialogOpen: true,
+    });
+  };
+
+  closeDialog = () => {
+    this.setState({dialogOpen: false});
+  };
+
+  render() {
+    return (
+      <div>
+      <MuiThemeProvider>
+        <Dialog
+          title="Make Payment"
+          modal={true}
+          open={true}
+        >
+          <Card />
+        </Dialog>
+      </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
 @connect() // Only dispatches.
 class CartItem extends Component {
     constructor(props) {
         super(props)
-        
+
         this.state = {editing: false, quantity: this.props.quantity}
 
 
@@ -96,14 +243,14 @@ class CartItem extends Component {
                 {quantity}
                 <td>
                     <a href='#' onClick={this.handleDelete}>Delete</a>
-                </td>        
+                </td>
             </tr>
-        )    
+        )
     }
 }
 
 /***
- Looks up product from ID. 
+ Looks up product from ID.
  ***/
 const findProduct = (item, products) => {
     for(var i in products) {
@@ -179,7 +326,8 @@ class CartCost extends Component {
 
         this.state = {
             shippingMethod: null,
-            address: {}
+            address: {},
+            checkOut: false
         };
     }
 
@@ -319,15 +467,15 @@ class CartCost extends Component {
     goToCheckout() {
         if(!this.checkDetails())
             return false;
-        alert("Being dispatched.")
+        this.setState({checkOut: true});
     }
 
     renderCheckout() {
-        return this.state.shippingMethod == null ? (<span />) : (
+        return this.state.shippingMethod == null ? (<span />) : (!this.state.checkOut ? (
             <a className="button is-primary pull-right" onClick={this.goToCheckout}>
                 Place Order
             </a>
-        );
+          ) : (<Payment />));
     }
 
     render() {
@@ -383,7 +531,7 @@ class Cart extends Component {
         var contents = empty ? (
             <span>Empty.</span>
         ) : (
-            <CartTable /> 
+            <CartTable />
         );
 
         return (
@@ -391,7 +539,6 @@ class Cart extends Component {
                 <Title>Items in your Basket</Title>
                 {contents}
                 <CartCost />
-
             </Section>
         );
     }
