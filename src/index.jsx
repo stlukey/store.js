8bcd379d9c16fef848c41fae1cf4f2600a3c2594
Luke Southam <luke@devthe.com>
2017-08-29 15:58:52 +0100

    Further improvements to home page styling.

--- src/index.jsx
+++ src/index.jsx
@@ -39,7 +39,7 @@ const unlisten = GA_TRACKING_CODE ? history.listen(location => {
 }) : null;
 
 window.API = API_URL;
-window.SITE_TITLE = "Maryam's \n Persian Pantry";
+window.SITE_TITLE = "Maryam's \nPersian Pantry";
 
 const routes = (
     <Route path="/" component={App}>
